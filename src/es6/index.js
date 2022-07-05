85839b0abe28d4015c6f405c8ad415adabfca085
Hernán Chamorro <hernang.work@gmail.com>
2022-07-05 19:10:07 -0300

    'multilinea_objectdestructuring_spread_operator_arrow_function_const_let_promises_object_parameters'

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,8 +1,7 @@
-// default params: 
+// default params:
 // establecer un valor por defecto a los parámetros de una función
-// nueva funcion para ver como se utilizaban los default params antes
+
 function newFunction(name, age, country, params) {
-    // inicializando
     var name = name || 'hernan';
     var age = age || 27;
     var country = country || 'argentina';
@@ -15,17 +14,18 @@ function newFunction2(name = 'hernan', edad = 27, country = 'argentina',) {
     console.log(name, edad, country);
 }
 
-// llamado de la funcion
-// si no le paso los parametros los toma por defecto
 newFunction2();
 newFunction2('Ricardo', '23', 'Colombia')
+
 //================================================================
 
-// Template literals + con comillas francesas:
+// Template literals 
+// con comillas francesas:
 // crear cadenas de carácters que puedan contener variables
 
 let hello = "Hello"
 let world = "World"
+
 // metodo de concatenacion para unir palabras
 let epicPhrase = hello + ' ' + world
 console.log(epicPhrase)
@@ -34,5 +34,116 @@ console.log(epicPhrase)
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
 
+//================================================================
+
+// Multilínea
+// antes
+let lorem = "Texto default \n"
+    + "Otro texto";
+
+// es6
+let lorem2 = `Otra frase para practicar 
+ahora puedo ver como la multilínea
+existe  en el navegador`;
+
+console.log(lorem)
+console.log(lorem2);
+ //================================================================
+
+// Desestructuración de objetos
+// antes
+let person = {
+    'name': 'hernan',
+    'age': 27,
+    'country': 'AR'
+}
+
+console.log(person.age, person.name);
+// es6
+let { age, name, country } = person;
+console.log(name, age, country);
+//================================================================
+
+// unir arrays y añadir elementos con el spread operator
+let team1 = ['hernan', 'ricardo', 'juan'];
+let team2 = ['jose', 'maria', 'juan'];
+
+// estructuro teams y los uno
+let education = ['Bob', ...team1, ...team2];
+console.log(education);
 
 //================================================================
+// const / let : scope de bloque
+{ 
+    var global = "GLOBAL variable";
+
+}
+{
+    let globalLET = "GLOBAL variable2";
+    console.log(globalLET);
+}
+console.log(global);
+console.log(globalLET);
+//================================================================
+
+// Const: no se puede reasignar ni redeclarar
+// let: se puede reasignar pero no se puede re declarar
+
+const a = 'b';
+
+// reasignar - error
+// a = 'c';
+
+//================================================================
+
+// Arrow functions, promises y object parameters
+
+
+//Object parameters
+let nombre = 'hernan';
+let edad = 32; 
+
+ // antes
+obj = { name: nombre, age: edad };
+
+// ahora
+obj2 = { nombre, edad }
+
+
+// Arrow functions
+const names = [
+    { name: 'hernan', age: 32 },
+    { name: 'ricardo', age: 27 },
+    { name: 'juan', age: 23 }
+]
+
+//es5
+let listOfNames = names.map(function (item) {
+    console.log(item.name);
+})
+
+// es6
+let listofNames = names.map((item) => {
+    console.log(item.name)
+})
+
+// Promises
+
+const helloPromise = () => {
+    // nueva promesa, devuelve resolve o reject
+    return new Promise((resolver, reject) => {
+        if (true) {
+            resolver('Lo lograste!')
+        } else (
+            reject('Ups!')
+        )
+    })
+}
+
+helloPromise().then((response) => {
+    console.log(response);
+}
+).catch((error) => {
+    console.log(error);
+}
+)
