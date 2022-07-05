5ca79cca3b00c388a318f2ce06d84198053e3d6f
Hernán Chamorro <hernang.work@gmail.com>
2022-07-05 19:57:55 -0300

    'es7-includes

---
+++ src/es7/index.js
@@ -0,0 +1,11 @@
+// includes
+// para saber si un valor se encuentra dentro de un array o string
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+if (numbers.includes(7)) {
+    console.log('El valor se encuentra en el arreglo');
+} else {
+    console.log('El valor no se encuentra en el arreglo');
+}
+
