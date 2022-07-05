f2596eb195a3d0746065935e665e129486529998
Hernán Chamorro <hernang.work@gmail.com>
2022-07-05 20:46:21 -0300

    'finally'

--- src/es9/index.js
+++ src/es9/index.js
@@ -4,6 +4,38 @@ const obj = {
     country: 'AR'
     };
 
-// agrupar el residuo de elementos mediante ...
+// agrupar el residuo de elementos mediante el parametro rest ...
+// siempre se encuentra al final
 let { name, ...all } = obj;
 console.log(name, all)
+
+
+// operador de propagación expande los elementos de un iterable
+// en un array y no importa el lugar en el que este situado
+const obj1 = {
+    ...obj,
+    name: 'juan',
+    age: 22,
+    country: 'AR'
+}
+
+// finally en Promises
+// ejecutar codigo despues de que una promesa haya sido ejecutada como
+// resuelta o rechazada
+
+const helloWorld = () => {
+    return new Promises((resolve, reject) => {
+        (true)
+            ? resolve('Hello World!')
+            : reject(new Error('Test error'))
+    });
+}
+
+helloWorld().finally(() => {
+    console.log('Terminé!');
+})
+
+
+// Expresiones regulares - Regex
+// patrones de búsqueda y manipulación de cadenas de caracteres
+// se crea el patrón entre barras inclinadas (/patrón/)
