cc344fff2fbf0d7098d735531ad4b8d540a01afe
Hernán Chamorro <hernang.work@gmail.com>
2022-07-05 21:16:26 -0300

    'multiples_promesas'

--- src/ES11/index.js
+++ src/ES11/index.js
@@ -17,3 +17,22 @@ button.addEventListener('click', async (event) => {
 const number1 = 45n
 const number2 = BigInt(45)
 
+// manejo de varias Promises
+// se utilizar Promise.all(), recibe como argumento un array de promseas
+// pero importa el estado de las promesas
+// si hay una rejected, se para la ejecución
+// en cambio con allSettled se sigue ejecutando sin importar el estado de las promesas
+
+const promise1 = new Promise((resolve, reject) => reject('reject'))
+const promise2 = new Promise((resolve, reject) => resolve('resolve1'))
+const promise3 = new Promise((resolve, reject) => resolve('resolve2'))
+
+
+Promise.allSettled([promise1, promise2, promise3]).then((response) => {
+    console.log(response)
+}
+).catch((error) => {
+    console.log(error)
+}
+)
+
